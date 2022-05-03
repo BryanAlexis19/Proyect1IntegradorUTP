4d2fde4e0aeb4c69fff1f29cb8ea5332226e6de9
Wytri <trizzalle@gmail.com>
2022-05-03 17:21:28 -0500

    Actualizacion

--- Idea/src/main/java/Entidades/Formula.java
+++ Idea/src/main/java/Entidades/Formula.java
@@ -12,7 +12,90 @@ public class Formula {
         String vec[]={"","DD","DI","II"};
         return vec[Tipo];
     }
-    
+    public void Derivar1(int num1, int expo1){
+        //gn1 , gexpo1 y gx1 son los parametros que ingresan desde afuera
+        //Prueba con un solo termino
+        //Si no se especific la variable y el exponente, se suponen como 1
+        if(expo1 == 0){
+            expo1 = 1;
+        }
+        
+        num1 = num1 * expo1;
+        expo1 = expo1 - 1;
+        
+        String phrase1 = (String.valueOf(num1) + "X^" + String.valueOf(expo1));
+        System.out.println(phrase1);
+    }
+    public void Derivar2(int num2, int expo2){
+        //gn1 , gexpo1 y gx1 son los parametros que ingresan desde afuera
+        //Prueba con un solo termino
+        //Si no se especific la variable y el exponente, se suponen como 1
+        if(expo2 == 0){
+            expo2 = 1;
+        }
+        
+        num2 = num2 * expo2;
+        expo2 = expo2 - 1;
+        
+        String phrase1 = (String.valueOf(num2) + "X^" + String.valueOf(expo2));
+        System.out.println(phrase1);
+    }
+    public void Derivar3(int num3, int expo3){
+        //gn1 , gexpo1 y gx1 son los parametros que ingresan desde afuera
+        //Prueba con un solo termino
+        //Si no se especific la variable y el exponente, se suponen como 1
+        if(expo3 == 0){
+            expo3 = 1;
+        }
+        
+        num3 = num3 * expo3;
+        expo3 = expo3 - 1;
+        
+        String phrase1 = (String.valueOf(num3) + "X^" + String.valueOf(expo3));
+        System.out.println(phrase1);
+    }
+    public void Integral1(int num1, int expo1){
+        //gn1 , gexpo1 y gx1 son los parametros que ingresan desde afuera
+        //Prueba con un solo termino
+        //Si no se especific la variable y el exponente, se suponen como 1
+        if(expo1 == 0){
+            expo1 = 1;
+        }
+        
+        num1 = (num1/expo1) * expo1;
+        expo1 = expo1 + 1;
+        
+        String phrase1 = (String.valueOf(num1) + "X^" + String.valueOf(expo1));
+        System.out.println(phrase1);
+    }
+    public void Integral2(int num2, int expo2){
+        //gn1 , gexpo1 y gx1 son los parametros que ingresan desde afuera
+        //Prueba con un solo termino
+        //Si no se especific la variable y el exponente, se suponen como 1
+        if(expo2 == 0){
+            expo2 = 1;
+        }
+        
+        num2 = (num2/expo2) * expo2;
+        expo2 = expo2 + 1;
+        
+        String phrase1 = (String.valueOf(num2) + "X^" + String.valueOf(expo2));
+        System.out.println(phrase1);
+    }
+    public void Integral3(int num3, int expo3){
+        //gn1 , gexpo1 y gx1 son los parametros que ingresan desde afuera
+        //Prueba con un solo termino
+        //Si no se especific la variable y el exponente, se suponen como 1
+        if(expo3 == 0){
+            expo3 = 1;
+        }
+        
+        num3 = (num3/expo3) * expo3;
+        expo3 = expo3 + 1;
+        
+        String phrase1 = (String.valueOf(num3) + "X^" + String.valueOf(expo3));
+        System.out.println(phrase1);
+    }
     public int getTipo() {
         return Tipo;
     }
