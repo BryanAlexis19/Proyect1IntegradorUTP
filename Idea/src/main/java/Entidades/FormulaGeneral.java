12a458cd2c9dbde5334bc63f867ebd37bced40c9
Wytri <trizzalle@gmail.com>
2022-05-15 14:14:54 -0500

    New

--- Idea/src/main/java/Entidades/FormulaGeneral.java
+++ Idea/src/main/java/Entidades/FormulaGeneral.java
@@ -1,6 +1,7 @@
 
 package Entidades;
 import java.util.*;
+import javax.swing.JTextArea;
 public class FormulaGeneral {
     //DECLARAR LAS VARIABLES DE LA CLASE
     //Termino independiente de la formula
@@ -168,5 +169,57 @@ public class FormulaGeneral {
         double resT;        
         resT = n1 + (n2 * Math.pow(var, expo2)) + (n3 * Math.pow(var, expo3));        
         return resT;
+    }
+    public void Procedimiento(JTextArea ar){
+        double r=0;
+        double r2=0;
+        double N=0;
+        double N2=0;
+        double NE=0;
+        double NE2=0;
+        int Tipo=0;
+        int T=var;
+        if(n1>0 && T>0){
+            ar.append("\n---PROCEDIMIENTO(P/T)-------------------------------");
+            ar.append("\n "+n1+" + "+n2+" * "+" ("+T+")^ "+expo2+" + "+n3+" * "+" ("+T+")^ "+expo3);
+            N=n2*Math.pow(T, expo2);
+            N2=n3*Math.pow(T, expo3);
+            ar.append("\n "+n1+" + "+N+" + "+N2);
+            ar.append("\n----------------------------------------------------");
+            ar.append("\n--------DESPLAZAMIENTO------------------------------");
+            ar.append("\n ("+n1+" + "+n2+" * "+" ("+T+")^ "+expo2+" + "+n3+" * "+" ("+T+")^ "+expo3+")"+" - "+n1+" + "+n2+" * "+" ("+0+")^ "+expo2+" + "+n3+" * "+" ("+0+")^ "+expo3);
+            ar.append("\n ("+n1+" + "+N+" + "+N2+")"+" - "+n1);
+            r=n1+N+N2;
+            ar.append("\n ("+r+")"+" - "+n1);
+            ar.append("\n----------------------------------------------------");
+        }
+        else if(n2>0 && n1==0){
+            ar.append("\n---PROCEDIMIENTO(D)---------------------------------");
+            ar.append("\n "+n1+" * "+0+" + ("+n2+" * "+expo2+") X^"+expo2+" - "+1+" + ("+n3+" * "+expo3+") X^"+expo3+" - "+1);
+            N=n2*expo2;
+            NE=expo2-1;
+            N2=n3*expo3;
+            NE2=expo3-1;
+            ar.append("\n "+N+" X^"+NE+" + "+N2+" X^"+NE2);
+            ar.append("\n----------------------------------------------------");
+            if(T>0){//errores esta bien pero a la vez mal haha
+            ar.append("\n---PROCEDIMIENTO(V/T)-------------------------------");
+            ar.append("\n "+n1+" * "+0+" + ("+n2+" * "+expo2+") X^"+expo2+" - "+1+" + ("+n3+" * "+expo3+") X^"+expo3+" - "+1);
+            ar.append("\n "+N+" * "+ T +" ^"+NE+" + "+N2+" * "+ T +" ^"+NE2);
+            r=Math.pow(T, NE);
+            r2=Math.pow(T, NE2);
+            ar.append("\n "+N+" * "+ r +" + "+N2+" * "+r2);
+            ar.append("\n----------------------------------------------------");
+            }
+        /*}else if(n2>0 && n3>0 && n1<0){
+            ar.append("\n---PROCEDIMIENTO--------------------------------------");
+            ar.append("\n "+r+" * "+0+" + ("+n2+" * "+expo2+") X^"+expo2+" - "+1+" + ("+n3+" * "+expo3+") X^"+expo3+" - "+1);
+            N=n2*expo2;
+            NE=expo2-1;
+            N2=n3*expo3;
+            NE2=expo3-1;
+            ar.append("\n "+N+" X^"+NE+" + "+N2+" X^"+NE2);
+            ar.append("\n-------------------------------------------------------");*/
+        }
     }
 }
